395dca8afedf603a405729b219de41c9f3cc5f5a
chloeprowse <chloeprowse1@gmail.com>
2025-11-18 01:08:17 +0000

    Deploying to gh-pages from @ chloeprowse/chloeprowse.github.io@badcac270ba540e2d2e0c3714516fba586b0a176 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -496,6 +496,11 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-",
+          title: '',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/CaseStudy1DDS/";
             },},{
         id: 'social-cv',
         title: 'CV',
